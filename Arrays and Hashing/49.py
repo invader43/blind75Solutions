152293924ba72763b6e8954405e5e3b694c54d25
invader43 <55555dakkale@gmail.com>
2024-06-06 18:58:22 +0530

    Optimized Group Anagrams

--- Arrays and Hashing/49.py
+++ Arrays and Hashing/49.py
@@ -1,21 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         #datastructures used , array and hashmap
-        sortedstrs = []
         hashmap = {}
         finallist = []
 
-        #sort all individual strings
-        for word in strs:
+        #sort individual strings
+        for i,word in enumerate(strs):
             sortedword = sorted(list(word))
-            sortedstrs.append(''.join(sortedword))
-
+            sortedword = ''.join(sortedword)
         #now get indices that are equal , hashmap.get , and if it returns none , create a new key
-        for i,srtedstr in enumerate(sortedstrs):
-            if srtedstr in hashmap:
-                hashmap[srtedstr].append(i)
+            if sortedword in hashmap:
+                hashmap[sortedword].append(i)
             else :
-                hashmap[srtedstr] = [i]
+                hashmap[sortedword] = [i]
 
         #use keys and original list of strings to create the groups of anagrams
         for key in hashmap.keys():
--- README.md
+++ README.md
@@ -9,6 +9,3 @@ Solutions to leetcode solutions sorted by algorithm name , authored by Daksesh.
 [242.Valid Anagram](https://github.com/invader43/blind75Solutions/blob/main/Arrays%20and%20Hashing/242.py)\
 [1.Two Sum](https://github.com/invader43/blind75Solutions/blob/main/Arrays%20and%20Hashing/1.py)\
 [49.Group Anagrams](https://github.com/invader43/blind75Solutions/blob/main/Arrays%20and%20Hashing/49.py)
-
-
-Optimize the solution for group anagrams.
