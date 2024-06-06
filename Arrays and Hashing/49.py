eef58695f88d513ff18310c0e6d7df3d895612c6
invader43# <55555dakkale@gmail.com>
2024-06-06 16:16:43 +0530

    Added Group Anagrams

---
+++ Arrays and Hashing/49.py
@@ -0,0 +1,29 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #datastructures used , array and hashmap
+        sortedstrs = []
+        hashmap = {}
+        finallist = []
+
+        #sort all individual strings
+        for word in strs:
+            sortedword = sorted(list(word))
+            sortedstrs.append(''.join(sortedword))
+
+        #now get indices that are equal , hashmap.get , and if it returns none , create a new key
+        for i,srtedstr in enumerate(sortedstrs):
+            if srtedstr in hashmap:
+                hashmap[srtedstr].append(i)
+            else :
+                hashmap[srtedstr] = [i]
+
+        #use keys and original list of strings to create the groups of anagrams
+        for key in hashmap.keys():
+            finallist.append([strs[i] for i in hashmap[key]])
+
+        return finallist
+
+
+
+
+        
